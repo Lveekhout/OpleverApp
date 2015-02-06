f5d5d04479559138b8f3b67f9821b9cddd4e45d1
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2015-02-06 16:36:07 +0100

    Static test

---
+++ src/test/nl/lveekhout/statics/StaticClass.java
@@ -0,0 +1,10 @@
+package nl.lveekhout.statics;
+
+/**
+ * Created by eekhout.l on 06-02-2015.
+ * class StaticClass
+ */
+
+public class StaticClass {
+    public static String maxSpeed = "Laurens van Eekhout";
+}
---
+++ src/test/nl/lveekhout/statics/StaticTest.java
@@ -0,0 +1,12 @@
+package nl.lveekhout.statics;
+
+/**
+ * Created by eekhout.l on 06-02-2015.
+ * class StaticTest
+ */
+
+public class StaticTest {
+    public static void main(String[] args) {
+        System.out.println(StaticClass.maxSpeed);
+    }
+}
