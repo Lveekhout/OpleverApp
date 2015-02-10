57f386739653fa328d1e5c7c9bd9547ae45b52ac
Laurens van Eekhout <eekhout.l@tkppensioen.nl>
2015-02-10 13:34:53 +0100

    Regular expressions geupdate

--- src/test/nl/lveekhout/strings/TestRegularExpression.java
+++ src/test/nl/lveekhout/strings/TestRegularExpression.java
@@ -1,5 +1,8 @@
 package nl.lveekhout.strings;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by eekhout.l on 20-1-15.
  * class TestRegularExpression
@@ -7,7 +10,8 @@ package nl.lveekhout.strings;
 
 public class TestRegularExpression {
     public static void main(String[] args) {
-        System.out.println("abc.Ear".matches(".*\\.(?i)ear(?-i)"));
+        File file = new File(".");
+        System.out.println("X:\\ICT\\03 ICT Ontwikkeling\\02 ICT Bouw\\85 Teams\\PartyScene\\Sprint2015-03\\RGR_3.02.026\\Fmb\\pasf0850.fmb".matches(".*\\\\(?i)fmb(?-i)\\\\.*\\.(?i)fmb(?-i)"));
         System.out.println("Dumpfile versie 3.00".matches("(Dumpfile )sjaakversie.*"));
     }
 }
